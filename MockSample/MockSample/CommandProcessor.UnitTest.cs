a04e44613262be98705b227931845c093db5dd1a
crcrrajesh <crcrrajesh@gmail.com>
2017-07-19 16:29:13 -0400

    Moq sample

---
+++ MockSample/MockSample/CommandProcessor.UnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace MockSample
+{
+    [TestFixture]
+    public class CommandProcessorUnitTest
+    {
+       [Test]
+        public void SendCommand()
+        {
+            Mock<IFirmware> mockFirmware = new Mock<IFirmware>();
+            Action<IFirmware> fu=new Action<IFirmware>((ob)=>ob.IsOkToSendCommand("Start"));
+            //System.Linq.Expressions.Expression<Func<IFirmware,bool>> exp = (ob)=>ob.IsOkToSendCommand("Start");
+
+            mockFirmware.Setup(ob => ob.IsOkToSendCommand("Start")).Returns(() => true);
+           
+            CommandProcessor commandProcessor = new CommandProcessor(mockFirmware.Object);
+            Assert.AreEqual(1,commandProcessor.SendCommand("Start"));
+            mockFirmware.Verify(ob=>ob.IsOkToSendCommand("Start"),Times.AtLeast(1),"DD");
+        }
+    }
+}
---
+++ MockSample/MockSample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public interface IFirmware
+    {
+        void SendCommand(string name);
+        bool IsOkToSendCommand(string name);
+        string GetLastExecutedCommand();
+    }
+
+    public class CommandProcessor
+    {
+        private readonly IFirmware _firmware;
+
+        public CommandProcessor(IFirmware firmware)
+        {
+            _firmware = firmware;
+        }
+
+       
+
+        public int SendCommand(string name)
+        {
+            if (_firmware.IsOkToSendCommand(name))
+            {
+                _firmware.SendCommand(name);
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
