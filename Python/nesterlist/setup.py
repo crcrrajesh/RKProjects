9a8956f63507deabdb5043f95eff3ab097abfdbc
crcrrajesh <crcrrajesh@gmail.com>
2017-10-05 11:24:18 -0400

    Python Project added

---
+++ Python/nesterlist/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='nesterd',
+    version='1.0.1',
+    packages=[''],
+    url='',
+    license='PY',
+    author='rajesh.kumar',
+    author_email='',
+    description='nesterdes'
+)
